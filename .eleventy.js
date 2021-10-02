909bd8866564878a2e418a2335e2580580b8ea74
Reed Piernock <18013388+reedcodes@users.noreply.github.com>
2021-10-01 20:48:31 -0400

    set up flickr grid shortcodes

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,22 @@ module.exports = function (eleventyConfig) {
   } );
   eleventyConfig.addPassthroughCopy( "./source/assets/images/" );
 
+  eleventyConfig.addPairedShortcode( 'flickrgrid', ( content ) => {
+    return `<div class="flickr-grid">${content}</div>`;
+  } );
+
+  eleventyConfig.addShortcode( 'flickr', ( user, id, source, width, height, title, caption ) => {
+    return `<figure class="flickr-item">
+      <a data-flickr-embed="true" href="https://www.flickr.com/photos/${user}/${id}/">
+        <img src="https://live.staticflickr.com/65535/${source}.jpg" width="${width}" height="${height}" alt="${title}">
+      </a>
+      <figcaption>
+        <p id="photo-${id}">${caption}</p>
+        <a aria-labelledby="photo-${id}" href="https://www.flickr.com/photos/${user}/${id}/">view on flickr</a>
+      </figcaption>
+    </figure>`;
+  } );
+
   eleventyConfig.addShortcode( 'icon', ( value ) => {
     const style = value.style ? value.style : "fas";
     return `<span class="${style} fa-${value.name}" aria-hidden="true"></span>`;
