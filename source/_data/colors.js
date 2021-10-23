f57b50455c6f993686adf596a8c7567da656d0b7
Reed Piernock <18013388+reedcodes@users.noreply.github.com>
2021-10-23 16:13:59 -0400

    set up dark theme colors

--- source/_data/colors.js
+++ source/_data/colors.js
@@ -1,8 +1,13 @@
 module.exports = [
   {
     // beige
-    name: "light",
+    name: "main",
     color: "#f4f4e1"
+  },
+  {
+    // black-green
+    name: "contrast",
+    color: "#173c39"
   },
   {
     // straw
@@ -25,8 +30,33 @@ module.exports = [
     color: "#bea467",
   },
   {
-    // black-green
-    name: "dark",
-    color: "#173c39"
+    // jungle green
+    name: "dark-main",
+    color: "#0d1c1b"
+  },
+  {
+    // burlywood
+    name: "dark-contrast",
+    color: "#d7b784"
+  },
+  {
+    // bistre
+    name: "dark-highlight",
+    color: "#76712d"
+  },
+  {
+    // blood
+    name: "dark-primary",
+    color: "#621711"
+  },
+  {
+    // dark teal
+    name: "dark-secondary",
+    color: "#40705f"
+  },
+  {
+    // field drab
+    name: "dark-accent",
+    color: "#66502c",
   }
 ];
