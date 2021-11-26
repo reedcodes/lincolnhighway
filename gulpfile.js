71adbbc2a8baf9b387c8d06ec6705a1ca5fdf300
Reed Piernock <18013388+reedcodes@users.noreply.github.com>
2021-11-26 16:36:28 -0500

    update css and scripts to match reedcodes

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const gulp = require( 'gulp' ),
-      sass = require( 'gulp-dart-sass' );
+      sass = require( 'gulp-sass' )( require( 'sass' ) );
 
 // Define CSS source and distribution directories.
 const cssSource = './source/_sass/**/*.scss';
@@ -9,7 +9,8 @@ const cssBuild = './site/dist/css';
 
 // Define CSS source paths from other locations, e.g. node modules.
 const cssIncludePaths = [
-  './node_modules/@fortawesome/fontawesome-free/scss'
+  './node_modules/@fortawesome/fontawesome-free/scss',
+  './node_modules/include-media/dist'
 ];
 
 // Task to compile CSS files.
