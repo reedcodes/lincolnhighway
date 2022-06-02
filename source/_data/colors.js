6b48109a62cd368486257577e8997d0d73d0378d
Reed Piernock <code@reedcodes.com>
2022-06-02 15:29:01 -0400

    add colors and color tokens

---
+++ source/_data/colors.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    // Lace white.
+    name: "primary",
+    color: "hsla( 45, 100%, 96%, 1 )"
+  },
+  {
+    // Golden yellow.
+    name: "secondary",
+    color: "hsla( 47, 99%, 50%, 1 )"
+  },
+  {
+    // Burnt orange.
+    name: "accent",
+    color: "hsla( 18, 61%, 33%, 1 )"
+  },
+  {
+    // Teal.
+    name: "highlight",
+    color: "hsla( 188, 63%, 45%, 1 )"
+  },
+  {
+    // Navy.
+    name: "contrast",
+    color: "hsla( 195, 100%, 8%, 1 )"
+  }
+];
