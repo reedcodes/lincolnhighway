599d96b2a7d77a696f403149cf860eafa5857fec
Reed Piernock <code@reedcodes.com>
2022-08-29 18:37:48 -0400

    add option for flickr in image shortcode/plugin

--- source/_config/shortcodes/image.js
+++ source/_config/shortcodes/image.js
@@ -4,32 +4,40 @@ const Image = require( "@11ty/eleventy-img" );
 // Configure the image plugin.
 // https://www.11ty.dev/docs/plugins/image/
 // https://gfscott.com/blog/eleventy-img-without-central-image-directory/
+
 module.exports = async function( src, alt, flickr=false, sizes="100vw" ) {
   if(alt === undefined) {
     throw new Error(`Missing \`alt\` on: ${src}`);
   }
 
-  // The input path always ends with `xx.md` as the post's filename. We can
-  // slice this off to get the proper path for the image's directory.
-  let inputDirectory = this.page.inputPath.slice(0, -5);
-
-  // The output path always ends with `index.html` as the post's filename. We
-  // can slice that off to get the proper path for the generated image's
-  // directory.
-  let outputDirectory = this.page.outputPath.slice(0, -10);
-
-
-
+  // Define the image source. This will depend on where the image is from:
+  // a local file within the blog post, or an external flickr image.
   let imageSrc;
 
+  // The default for flickr (set in parameters) is false. This is only set if
+  // the source of the image comes from flickr.
   if(flickr) {
+    imageSrc = src;
   }
 
+  // The default for the image source is a local file within the blog post.
+  // So, if it's not a flickr image (default), then we use the following.
   else {
-    // Create the source path of the image by joining the updated input directory
-    // with the filename of the image itself.
+    // The input path always ends with `xx.md` as the post's filename. We can
+    // slice this off to get the proper path for the image's directory.
+    let inputDirectory = this.page.inputPath.slice(0, -5);
+    
+    // Create the source path of the image by joining the updated input
+    // directory with the filename of the image itself.
     imageSrc = `${inputDirectory}/${src}`;
   }
+
+  // Images have an output directory based on which blog post they are
+  // associated with, so that the images live within the same directory as their
+  // posts, for easy finding. The output path always ends with `index.html` as
+  // the post's filename. We can slice that off to get the proper path for the
+  // generated image's directory.
+  let outputDirectory = this.page.outputPath.slice(0, -10);
 
   // Set the metadata for this image, that 11ty will generate.
   let metadata = await Image(imageSrc, {
@@ -42,6 +50,8 @@ module.exports = async function( src, alt, flickr=false, sizes="100vw" ) {
   let lowsrc = metadata.jpeg[0];
   let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
 
+  // Return the image. The output code is a `picture` with different source
+  // files, depending on the browser's size and what it supports.
   return `\n\n<picture>
     ${Object.values(metadata).map(imageFormat => {
       return `<source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(', ')}" sizes="${sizes}">`;
