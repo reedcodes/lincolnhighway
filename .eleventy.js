fe9f6a2f0cc5adaf9aa0aec5894526a8358a8cf8
Reed Piernock <18013388+reedcodes@users.noreply.github.com>
2022-09-06 23:31:58 -0400

    2022-09-06 merge test into main (#35)
    * update two test blog posts
    
    * Post for 2022-09-05 (#29)
    
    * write first blog post, update feature image
    
    * remove test blog posts, remove focus-visible
    
    * add smart quotes in md files
    
    * second blog post, site folder cleanup
    
    * add RSS feed to blog (#31)
    
    * Post for 2022-09-05: merge test into main (#30)
    
    * update two test blog posts
    
    * Post for 2022-09-05 (#29)
    
    * write first blog post, update feature image
    
    * remove test blog posts, remove focus-visible
    
    * add smart quotes in md files
    
    * second blog post, site folder cleanup
    
    * add RSS feed to the blog
    
    * add meta information to site (#32)
    
    * Post for 2022-09-05: merge test into main (#30)
    
    * update two test blog posts
    
    * Post for 2022-09-05 (#29)
    
    * write first blog post, update feature image
    
    * remove test blog posts, remove focus-visible
    
    * add smart quotes in md files
    
    * second blog post, site folder cleanup
    
    * add meta information for site, opengraph, etc

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
 // Import 11ty nav plugin.
 const eleventyNavigationPlugin = require( "@11ty/eleventy-navigation" );
 
+// Import metagen plugin.
+const metagen = require( "eleventy-plugin-metagen" );
+
+// Import 11ty RSS plugin.
+const eleventyRssPlugin = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function( eleventyConfig ) {
 
   // Send assets from source to site.
@@ -11,6 +17,19 @@ module.exports = function( eleventyConfig ) {
 
   // Add the 11ty nav plugin.
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+  // Add the metagen plugin.
+  eleventyConfig.addPlugin(metagen);
+
+  // Add the 11ty RSS plugin.
+  eleventyConfig.addPlugin(eleventyRssPlugin, {
+    posthtmlRenderOptions: {
+      quoteStyle: 0
+    }
+  });
+
+  eleventyConfig.addFilter( "getNewestCollectionItemDate", eleventyRssPlugin.getNewestCollectionItemDate );
+  eleventyConfig.addFilter( "dateToRfc822", eleventyRssPlugin.dateToRfc822 );
 
   // Add blog glob.
   eleventyConfig.addCollection( "blogPosts", require("./source/_config/collections/blog-posts.js") );
